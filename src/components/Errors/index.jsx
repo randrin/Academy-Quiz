f42c02dfcd64cb40e2b2fb65fa0ccf6419bff703
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-07-27 18:35:25 +0200

    Error Page Implementation

--- src/components/Errors/index.jsx
+++ src/components/Errors/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import ErrorImage from "../../images/batman.png";
 
 const ErrorPage = () => {
+  const errorMessage = {
+    textAlign: "center",
+    marginTop: "50px",
+  };
+  const errorImage = {
+    display: "block",
+    margin: "50px auto",
+  };
   return (
     <div className="quiz-bg">
-      <div className="container">Error Page</div>
+      <div className="container">
+        <h2 style={errorMessage}>Error Page</h2>
+        <img src={ErrorImage} style={errorImage} alt="Error Page" />
+      </div>
     </div>
   );
 };
