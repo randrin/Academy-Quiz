12dc5d710170b25fe0f1b37394545d3b1453a09f
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-07-28 10:21:09 +0200

    Add Back redirection to Error Page

--- src/components/Errors/index.jsx
+++ src/components/Errors/index.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaAngleLeft } from "react-icons/fa"
 import ErrorImage from "../../images/batman.png";
 
 const ErrorPage = () => {
@@ -13,8 +15,11 @@ const ErrorPage = () => {
   return (
     <div className="quiz-bg">
       <div className="container">
-        <h2 style={errorMessage}>Error Page</h2>
+        <h2 style={errorMessage}>404 Error Page</h2>
         <img src={ErrorImage} style={errorImage} alt="Error Page" />
+        <Link to="/" class="btn-redirection">
+          <FaAngleLeft className="btn-icon" />Back to Home
+        </Link>
       </div>
     </div>
   );
