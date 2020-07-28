ff9ff388d48fb1b73678400248210b4fc79a2c5d
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-07-28 17:15:01 +0200

    Session Authentification

--- src/components/Welcome/index.jsx
+++ src/components/Welcome/index.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { AcademyContext } from "../Firebase";
 import NavBar from "../NavBar";
 import Quiz from "../Quiz";
 
-const Welcome = () => {
-  return (
+const Welcome = (prop) => {
+  const [userIsLogged, setUserIsLogged] = useState(null);
+  const academyContext = useContext(AcademyContext);
+
+  useEffect(() => {
+    academyContext.auth.onAuthStateChanged((user) => {
+      user ? setUserIsLogged(user) : prop.history.push("/");
+    });
+  }, []);
+
+  return userIsLogged === null ? (
+    <>
+      <div className="loader"></div>
+      <p className="loaderText">Loading ...</p>
+    </>
+  ) : (
     <div className="quiz-bg">
       <div className="container">
         <NavBar />
