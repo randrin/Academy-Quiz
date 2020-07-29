b6a1b05b557574a08c4c2360ec4069aa67282be3
@nzeukangnimpar <nzeukangrandrin@gmail.com>
2020-07-29 18:19:51 +0200

    Fetch Questions Quizz

--- src/components/Questions/index.jsx
+++ src/components/Questions/index.jsx
@@ -1,19 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
 import { FaChevronRight } from "react-icons/fa";
+import { QuizQuestions } from "../../data/index";
+class Questions extends Component {
+  constructor(props) {
+    super(props);
 
-const Questions = () => {
-  return (
-    <div>
-      <h2>Question title</h2>
-      <p className="answerOptions">Reponse 1</p>
-      <p className="answerOptions">Reponse 2</p>
-      <p className="answerOptions">Reponse 3</p>
-      <p className="answerOptions">Reponse 4</p>
-      <button className="btnSubmit">
-        Suivant <FaChevronRight className="academy-quiz-icon" />
-      </button>
-    </div>
-  );
-};
+    this.state = {
+      levels: ["debutant", "confirme", "expert"],
+      firstLevel: 0,
+      maxQuestions: 10,
+      storedQuestions: [],
+      question: "",
+      options: [],
+      idQuestion: 0,
+    };
+  }
+
+  loadQuestions = (level) => {
+    const allQuizz = QuizQuestions[0].quizz[level];
+    if (allQuizz.length >= this.state.maxQuestions) {
+      const questionsWithoutAnswers = allQuizz.map(
+        ({ answer, ...questions }) => questions
+      );
+      this.setState({
+        storedQuestions: questionsWithoutAnswers,
+      });
+    } else {
+      console.log(
+        "Pas assez de question pour ce niveau. Veillez patienter ...."
+      );
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levels[this.state.firstLevel]);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+  }
+
+  render() {
+    const { question, options } = this.state;
+    return (
+      <div>
+        <h2>{question}</h2>
+        {options.map((option, index) => {
+          return (
+            <p key={index} className="answerOptions">
+              {option}
+            </p>
+          );
+        })}
+        <button className="btnSubmit">
+          Suivant <FaChevronRight className="academy-quiz-icon" />
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Questions;
